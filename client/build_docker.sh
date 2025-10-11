5125bd0415f1eacf9908a5cb90566d93e3731986
roronoa_zoro <roronoazorod@163.com>
2025-10-11 15:41:37 +0800

    fix bug

--- client/build_docker.sh
+++ client/build_docker.sh
@@ -1,5 +1,7 @@
 
 
-sudo docker build -t AudioAnno .
+sudo docker build -t audio_anno .
 
-sudo docker save -o AudioAnno.tar AudioAnno:lates
+sudo docker save -o audio_anno.tar audio_anno:latest
+
+sudo docker run -d --name audio_anno_v1 --add-host=host.docker.internal:host-gateway -p 8800:80 audio_anno
