aa845b4052c5c5b8e1f64c4332d9f9749b859fe6
roronoa_zoro <roronoazorod@163.com>
2025-10-14 20:51:02 +0800

    fix warning

--- client/src/DatasetDetail.js
+++ client/src/DatasetDetail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { data, useLocation, useNavigate, useParams } from 'react-router-dom';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import { useActiveDatasets } from './ActiveDatasetsContext';
 
 const DatasetDetail = () => {
