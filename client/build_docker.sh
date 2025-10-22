79b1719f21aff4f9958cd6cd21944fd8640e0864
roronoa_zoro <roronoazorod@163.com>
2025-10-22 09:17:34 +0800

    add space

--- client/build_docker.sh
+++ client/build_docker.sh
@@ -5,3 +5,4 @@ sudo docker build -t audio_anno .
 sudo docker save -o audio_anno.tar audio_anno:latest
 
 sudo docker run -d --name audio_anno_v1 --add-host=host.docker.internal:host-gateway -p 8800:80 audio_anno
+
